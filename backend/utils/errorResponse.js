a9201885467fa1903ca1252c94fad6bba5823bb9
Mishalsx <mishal.als@hotmail.com>
2025-04-12 12:39:52 +0300

    Add backend/utils/errorResponse.js 1744450791860

---
+++ backend/utils/errorResponse.js
@@ -0,0 +1,10 @@
+class ErrorResponse extends Error {
+  constructor(message, statusCode, code = null, details = null) {
+    super(message);
+    this.statusCode = statusCode;
+    this.code = code;
+    this.details = details;
+  }
+}
+
+module.exports = ErrorResponse;
